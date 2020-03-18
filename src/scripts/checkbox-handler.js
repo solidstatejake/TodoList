04e9e15d7a6621c9ad19f8298e4fc6966c5f77a1
solidstatejake <jberg3@binghamton.edu>
2020-03-18 14:46:42 -0400

    Remove useless comment.

--- src/scripts/checkbox-handler.js
+++ src/scripts/checkbox-handler.js
@@ -1,9 +1,3 @@
-// document.querySelectorAll( '.checkbox' ).forEach( checkbox => {
-//   console.log( 'I tagged a checkbox!' )
-//   checkbox.addEventListener( 'click', () => {
-//     checkbox.classList.toggle( 'checked' )
-//   } )
-// } )
 $( '#prioritize-option' ).click( function () {
   $( this ).toggleClass( 'checked' )
 } )
